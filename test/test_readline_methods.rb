a5d0be94644c61127b3514c486ade42ff45b1f6d
Zach Aysan <zachaysan@gmail.com>
2018-11-30 09:20:22 -0800

    Update Readline test to test module

--- test/test_readline_methods.rb
+++ test/test_readline_methods.rb
@@ -4,7 +4,7 @@ require "vv"
 class ReadlineMethodsTest < Minitest::Test
 
   def test_readline_included_vv
-    assert Readline.vv_included?
+    assert Readline::vv_enabled?
   end
 
   # TODO: Implement tests for prompt methods
