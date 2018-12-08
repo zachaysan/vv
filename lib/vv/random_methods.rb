5f4901dea7bc9a977a13dbe3138b5728babcc33e
Zach Aysan <zachaysan@gmail.com>
2018-12-08 12:36:52 -0800

    Create random methods

---
+++ lib/vv/random_methods.rb
@@ -0,0 +1,56 @@
+module VV
+
+  module RandomClassSettings
+
+    def random_identifier_default_length
+      40
+    end
+
+  end
+
+  module RandomMethods
+
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+
+      def vv_included?
+        true
+      end
+
+      def default_charset
+        36
+      end
+
+      def random_identifier length=nil, charset_count: nil
+        length ||= self.random_identifier_default_length
+
+        charset_count ||= self.default_charset
+        largest_digit   = (charset_count - 1).to_s charset_count
+
+        start  = ( largest_digit * ( length - 1 ) ).to_i charset_count
+        finish = ( largest_digit * ( length     ) ).to_i charset_count
+        range  = start..finish
+
+        SecureRandom.random_number(range).to_s charset_count
+      end
+
+      def random_character *args, capitals: false
+        String.letters_and_numbers(capitals: capitals)
+          .sample(*args, random: SecureRandom)
+      end
+
+    end
+
+    def random_identifier *args, **kwargs
+      self.class.random_identifier( *args, **kwargs )
+    end
+
+    def letters_and_numbers
+      self.class.letters_and_numbers
+    end
+
+  end
+end
