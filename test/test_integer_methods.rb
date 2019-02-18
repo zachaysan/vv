b8942dc48a7855eb1d9ca712a15f1db1583982ff
Zach Aysan <zachaysan@gmail.com>
2019-02-18 10:27:28 -0800

    Test dashes and characters

--- test/test_integer_methods.rb
+++ test/test_integer_methods.rb
@@ -14,4 +14,21 @@ class IntegerMethodsTest < Minitest::Test
     assert_equal four, four.to_i!
   end
 
+  def test_dashes
+    assert_equal "-", 1.dashes
+    assert_equal "---", 3.dashes
+    assert_equal "", 0.dashes
+    assert_equal "", -0.dashes
+    assert_equal "", -4.dashes
+  end
+
+  def test_characters
+    assert_equal "@@@", 3.characters("@")
+    message = assert_raises(RuntimeError) {
+      -3.characters( "@", fail_on_negative: true )
+    }.message
+    expected_message = "Expected non-negative integer, not `-3`."
+    assert_equal expected_message, message
+  end
+
 end
