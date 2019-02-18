2f08b755074fbe88b0254d4bdf87bfd0d0c03f2e
Zach Aysan <zachaysan@gmail.com>
2019-02-18 10:29:22 -0800

    Test cli printable for generic object

--- test/test_object_methods.rb
+++ test/test_object_methods.rb
@@ -10,4 +10,14 @@ class ObjectMethodsTest < Minitest::Test
     refute Object.new.blank?
   end
 
+  def test_cli_printable
+    object = Object.new
+    message = assert_raises(NoMethodError) do
+      object.cli_printable
+    end.message
+    expected_message = \
+    "`cli_printable` requires `cli_print` on child class"
+    assert_equal expected_message, message
+  end
+
 end
