83590a1720bd5faa0e41e0bca0d461a709fe53f7
Zach Aysan <zachaysan@gmail.com>
2019-02-19 11:34:01 -0800

    Remove now-redundant array methods

--- lib/vv/array_methods.rb
+++ lib/vv/array_methods.rb
@@ -3,6 +3,7 @@ module VV
 
     def self.included(base)
       base.extend(ClassMethods)
+      base.include(VV::SetMethods::SetAndArrayMethods)
       base.attr_accessor :cli_print_separator
     end
 
@@ -12,42 +13,6 @@ module VV
         true
       end
 
-    end
-
-    def gravify
-      self.collect do |elem|
-        "`#{elem}`"
-      end
-    end
-
-    def gravify!
-      self.collect! do |elem|
-        "`#{elem}`"
-      end
-    end
-
-    def includes_any?(other)
-      raise TypeError, "Expecting array" unless other.is_a? Array
-      uother = other.uniq
-      uself  = self.uniq
-      (uself + uother).uniq.size < (uself.size + uother.size)
-    end
-
-    def include_any?(other)
-      self.includes_any?(other)
-    end
-
-    def stringify_collection grave: false
-      return self.gravify.stringify_collection if grave
-
-      return String.empty_string if self.blank?
-      return self.first          if self.size == 1
-      return self.join " and "   if self.size == 2
-
-      new_collection = self[0..-3]
-      back_fragment  = self[-2..-1].join ", and "
-      new_collection << back_fragment
-      new_collection.join ", "
     end
 
     def spaced
