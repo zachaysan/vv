7fb39bf763b0c02eb008a7fc926ccabc72fe0205
Zach Aysan <zachaysan@gmail.com>
2019-02-20 17:11:57 -0800

    Make lookup table case insensitivable

--- lib/vv/utility/lookup_table.rb
+++ lib/vv/utility/lookup_table.rb
@@ -1,14 +1,26 @@
 class LookupTable
 
-  attr_reader :canonnicals, :aliases, :data
+  attr_reader :canonnicals,
+              :aliases,
+              :data,
+              :ignore_case
 
-  def initialize
+  def initialize ignore_case: false
+    @ignore_case = ignore_case
     @canonicals = Hash.new
     @aliases = Hash.new
     @data = Hash.new
   end
 
   def alias( key:, to: )
+
+    if @ignore_case
+      key = key.downcase
+      to  =  to.downcase
+    end
+
+    return if key == to
+
     _ensure_alias_possible key
 
     @canonicals[key] = to
@@ -18,14 +30,17 @@ class LookupTable
   end
 
   def []= key, value
+    key = key.downcase if @ignore_case
     @data[ self.canonical key ] = value
   end
 
   def [] key
+    key = key.downcase if @ignore_case
     @data[ self.canonical key ]
   end
 
   def canonical key
+    key = key.downcase if @ignore_case
     @canonicals[key] || key
   end
 
@@ -34,6 +49,7 @@ class LookupTable
   end
 
   def include? key
+    key = key.downcase if @ignore_case
     @data.include?( self.canonical key )
   end
 
@@ -48,29 +64,31 @@ class LookupTable
     }
   end
 
-  # Make Object class method called `delegate_missing`?
-  def method_missing(method, *args, **kwargs, &block)
-    super
-  rescue NoMethodError
-    begin
-      if args.size > 0 && kwargs.size > 0
-        return self.to_h.public_send method, *args, **kwargs, &block
-      elsif args.size > 0
-        return self.to_h.public_send method, *args, &block
-      elsif kwargs.size > 0
-        return self.to_h.public_send method, **kwargs, &block
-      else
-        return self.to_h.public_send method, &block
-      end
-    rescue NoMethodError
-    end
-    raise
+  # Because case is ignorable we can't automatically
+  # delegate all methods, only those without arguments
+  def keys *args, **kwargs, &block
+    both = args.present? && kwargs.present?
+
+    return to_h.keys(*args, **kwargs, &block) if both
+    return to_h.keys(**kwargs, &block) if kwargs.present?
+    return to_h.keys(*args, &block) if args.present?
+
+    to_h.keys( &block )
   end
 
   def lookup_canonical key
+    key = key.downcase if @ignore_case
+
     return key if self.canonical_keys.include? key
 
     @canonicals[key]
+  end
+  alias_method :lookup_key, :lookup_canonical
+
+  def fuzzy_lookup_canonical key
+    raise NotImplementedError
+    key = key.downcase if @ignore_case
+    Dir.glob pattern
   end
 
   protected
