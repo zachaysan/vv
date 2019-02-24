59a9d5e51d81829ca645cbacfd512291fe051778
Zach Aysan <zachaysan@gmail.com>
2019-02-24 12:14:59 -0800

    Move integer methods into their own module.

--- lib/vv/integer_methods.rb
+++ lib/vv/integer_methods.rb
@@ -1,25 +1,39 @@
-class Integer
+module VV
+  module IntegerMethods
 
-  def spaces
-    characters String.space
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+
+      def vv_included?
+        true
+      end
+
+    end
+
+    def spaces
+      characters String.space
+    end
+
+    def dashes
+      characters String.dash
+    end
+
+    def characters character, fail_on_negative: false
+      message = "Expected single character, not #{character}."
+      fail ArgumentError, message if character.length > 1
+
+      message = "Expected non-negative integer, not `#{self}`."
+      fail message if self < 0 and fail_on_negative
+
+      ( self > 0 ) ? ( character * self ) : String.empty_string
+    end
+
+    def to_i!
+      self
+    end
+
   end
-
-  def dashes
-    characters String.dash
-  end
-
-  def characters character, fail_on_negative: false
-    message = "Expected single character, not #{character}."
-    fail ArgumentError, message if character.length > 1
-
-    message = "Expected non-negative integer, not `#{self}`."
-    fail message if self < 0 and fail_on_negative
-
-    ( self > 0 ) ? ( character * self ) : String.empty_string
-  end
-
-  def to_i!
-    self
-  end
-
 end
