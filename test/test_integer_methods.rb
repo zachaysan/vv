ba4a8a3eea2378ce76bfa8dcd96de5a771e15d1a
Zach Aysan <zachaysan@gmail.com>
2019-03-22 14:34:29 -0700

    Test integer byte helpers

--- test/test_integer_methods.rb
+++ test/test_integer_methods.rb
@@ -48,4 +48,59 @@ class IntegerMethodsTest < Minitest::Test
     end
   end
 
+  def test_byte_helpers
+    one_byte   = 1
+    eight_bits = 8
+    assert_equal one_byte, eight_bits.bits
+
+    seven_bits = 7
+    assert_equal one_byte, seven_bits.bits
+
+    two_bytes = 2
+    nine_bits = 9
+    assert_equal two_bytes, nine_bits.bits
+
+    one_kib_in_bytes = 1024
+    assert_equal one_kib_in_bytes, 1.kibibyte
+
+    two_kib_in_bytes = 2048
+    assert_equal two_kib_in_bytes, 2.kibibytes
+    assert_equal two_kib_in_bytes, 2.KiB
+
+    one_mib_in_bytes = 1024 ** 2
+    assert_equal one_mib_in_bytes, 1.mebibyte
+
+    two_mib_in_bytes = one_mib_in_bytes * 2
+    assert_equal two_mib_in_bytes, 2.mebibytes
+    assert_equal two_mib_in_bytes, 2.MiB
+
+    one_gib_in_bytes = 1024 ** 3
+    assert_equal one_gib_in_bytes, 1.gibibyte
+
+    two_gib_in_bytes = one_gib_in_bytes * 2
+    assert_equal two_gib_in_bytes, 2.gibibytes
+    assert_equal two_gib_in_bytes, 2.GiB
+
+    one_tib_in_bytes = 1024 ** 4
+    assert_equal one_tib_in_bytes, 1.tebibyte
+
+    two_tib_in_bytes = one_tib_in_bytes * 2
+    assert_equal two_tib_in_bytes, 2.tebibytes
+    assert_equal two_tib_in_bytes, 2.TiB
+
+    one_pib_in_bytes = 1024 ** 5
+    assert_equal one_pib_in_bytes, 1.pebibyte
+
+    two_pib_in_bytes = one_pib_in_bytes * 2
+    assert_equal two_pib_in_bytes, 2.pebibytes
+    assert_equal two_pib_in_bytes, 2.PiB
+
+    one_eib_in_bytes = 1024 ** 6
+    assert_equal one_eib_in_bytes, 1.exbibyte
+
+    two_eib_in_bytes = one_eib_in_bytes * 2
+    assert_equal two_eib_in_bytes, 2.exbibytes
+    assert_equal two_eib_in_bytes, 2.EiB
+  end
+
 end
