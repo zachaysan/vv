5ae4cb2e99bce1304213236baea8d44bf93fa107
Zach Aysan <zachaysan@gmail.com>
2019-03-22 14:37:45 -0700

    Modularize object method helpers and add setting attrs via

--- lib/vv/object_methods.rb
+++ lib/vv/object_methods.rb
@@ -1,57 +1,75 @@
-class Object
+module VV
 
-  alias_method :responds_to?, :respond_to?
+  module ObjectMethods
 
-  def blank?
-    respond_to?(:empty?) ? !!empty? : !self
-  end unless method_defined? :blank?
+    alias_method :responds_to?, :respond_to?
 
-  def cli_printable **kwargs
-    String.get_stdout { self.cli_print( **kwargs ) }
-  rescue NoMethodError
-    message = \
-    "`cli_printable` requires `cli_print` on child class"
-    fail NoMethodError, message
-  end unless method_defined? :cli_printable
+    def set_attrs_via( *attributes, document: )
+      attributes.flatten!
+      document.keys.to_set.includes_all! attributes
+      attributes.each do |attribute|
+        value  = document.fetch(attribute)
+        setter = attribute.setter_sym
+        insta  = attribute.insta_sym
+        if self.respond_to? setter
+          self.public_send setter, value
+        else
+          self.instance_variable_set insta, value
+        end
+      end
+    end
 
-  def present?
-    !blank?
-  end unless method_defined? :present?
+    def blank?
+      respond_to?(:empty?) ? !!empty? : !self
+    end unless method_defined? :blank?
 
-  def one_of? *collection, mixed: false, allow_unsafe: false
-    nested   = collection.first.is_a? Array
-    nested ||= collection.first.is_a? Hash
-    nested ||= collection.first.is_a? Set
+    def cli_printable **kwargs
+      String.get_stdout { self.cli_print( **kwargs ) }
+    rescue NoMethodError
+      message = \
+      "`cli_printable` requires `cli_print` on child class"
+      fail NoMethodError, message
+    end unless method_defined? :cli_printable
 
-    message = \
-    "Unexpected nested argument. If desired set `allow_unsafe: true`."
-    fail ArgumentError, message if nested unless allow_unsafe
+    def present?
+      !blank?
+    end unless method_defined? :present?
 
-    return collection.include? self if mixed
+    def one_of? *collection, mixed: false, allow_unsafe: false
+      nested   = collection.first.is_a? Array
+      nested ||= collection.first.is_a? Hash
+      nested ||= collection.first.is_a? Set
 
-    klass = self.class
-    ok = collection.reject {|s| s.is_a? klass }.blank?
+      message = \
+      "Unexpected nested argument. If desired set `allow_unsafe: true`."
+      fail ArgumentError, message if nested unless allow_unsafe
 
-    message = "Invalid types: #{klass} collection required."
-    fail ArgumentError, message unless ok
+      return collection.include? self if mixed
 
-    collection.include? self
+      klass = self.class
+      ok = collection.reject {|s| s.is_a? klass }.blank?
+
+      message = "Invalid types: #{klass} collection required."
+      fail ArgumentError, message unless ok
+
+      collection.include? self
+    end
+
+    def one_of! *collection, mixed: false
+      return true if self.one_of?( *collection, mixed: mixed )
+
+      klass = self.class
+      collection = collection.stringify_collection grave: true
+      message = "#{klass} `#{self}` is invalid. Must be one of: #{collection}."
+      fail message
+    end
+
+    def is_a! klass
+      message = \
+      "Expected `#{klass}` instance, not `#{self.class}`."
+      fail ArgumentError, message unless self.is_a? klass
+    end
+    alias_method :must_be_a!, :is_a!
+
   end
-
-  def one_of! *collection, mixed: false
-    return true if self.one_of?( *collection, mixed: mixed )
-
-    klass = self.class
-    collection = collection.stringify_collection grave: true
-    message = "#{klass} `#{self}` is invalid. Must be one of: #{collection}."
-    fail message
-  end
-
-  def is_a! klass
-    message = \
-    "Expected `#{klass}` instance, not `#{self.class}`."
-    fail ArgumentError, message unless self.is_a? klass
-  end
-  alias_method :must_be_a!, :is_a!
-
 end
