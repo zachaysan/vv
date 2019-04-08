faa2af2ba6573271e8ca40c2ed56e87aefbdda39
Zach Aysan <zachaysan@gmail.com>
2019-04-08 14:40:16 -0700

    Create consider and related methods and properties

--- lib/vv/object_methods.rb
+++ lib/vv/object_methods.rb
@@ -4,6 +4,69 @@ module VV
 
     alias_method :responds_to?, :respond_to?
 
+    def consider input, &block
+      @__vv_consideration_dismissed_level ||= false
+
+      # For a potential parent consideration
+      return if self._consideration_dismissed?
+
+      @__vv_consideration_others ||= Hash.new
+      @__vv_consideration_level  ||= -1
+      @__vv_consideration_level   +=  1
+
+      @__vv_considerations ||= Array.new
+      @__vv_considerations << input
+      yield
+    ensure
+      if @__vv_consideration_dismissed_level == \
+         @__vv_consideration_level
+        remove_instance_variable :@__vv_consideration_dismissed_level
+      end
+
+      @__vv_consideration_level -= 1
+      if @__vv_consideration_level < 0
+        remove_instance_variable :@__vv_consideration_level
+        remove_instance_variable :@__vv_consideration_others
+      end
+      @__vv_considerations.pop
+      unless @__vv_considerations.any?
+        remove_instance_variable :@__vv_considerations
+      end
+    end
+
+    def given potential_match
+      return if self._consideration_dismissed?
+      return unless potential_match == self._consideration
+
+      response = yield
+      self._dismiss_consideration
+      response
+    end
+
+    def within potential_enum
+      return if self._consideration_dismissed?
+
+      return unless potential_enum.include? self._consideration
+      response = yield
+      self._dismiss_consideration
+      response
+    end
+
+    def otherwise
+      self._ensure_consideration_level!
+
+      level   = @__vv_consideration_level
+      message = "Multiple otherwise methods in consideration."
+      fail message if @__vv_consideration_others[level]
+      @__vv_consideration_others[level] = true
+
+      return if self._consideration_dismissed?
+
+      response = yield
+      self._dismiss_consideration
+      response
+    end
+
     def set_attrs_via( *attributes, document: )
       attributes.flatten!
       document.keys.to_set.includes_all! attributes
@@ -70,6 +133,43 @@ module VV
       fail ArgumentError, message unless self.is_a? klass
     end
     alias_method :must_be_a!, :is_a!
+
+    def _dismiss_consideration
+      self._ensure_consideration_level!
+      @__vv_consideration_dismissed_level = \
+      @__vv_consideration_level
+    end
+
+    def _consideration_dismissed?
+      defined = \
+      instance_variable_defined?("@__vv_consideration_dismissed_level")
+
+      message = \
+      "Assertion failure: @__vv_consideration_dismissed_level not defined"
+      fail message unless defined
+
+      !! @__vv_consideration_dismissed_level
+    end
+
+    def _ensure_consideration_level!
+      defined = instance_variable_defined?("@__vv_considerations")
+      fail "No current consideration active" unless defined
+
+      defined = instance_variable_defined?("@__vv_consideration_level")
+      message = "Assertion failure: Consideration level not set"
+      fail message unless defined
+
+      message = \
+      "Assertion failure: Consideration level mismatch."
+      size = @__vv_considerations.size
+      level_ok = size == ( @__vv_consideration_level + 1 )
+      fail message unless level_ok
+    end
+
+    def _consideration
+      self._ensure_consideration_level!
+      @__vv_considerations.last
+    end
 
   end
 end
